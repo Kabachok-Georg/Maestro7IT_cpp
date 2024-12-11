d5e0d875ee37099aa0c60c7a5f403551681fe62d
Георгий Данилов <addjorj3442@gmail.com>
2024-12-11 19:27:30 +0300

    new file:   .gitignore 	new file:   CPP/Lesson_1/index.cpp 	new file:   CPP/Lesson_1/lib_fstream.cpp 	new file:   CPP/Lesson_1/lib_iostream.cpp 	new file:   CPP/Lesson_1/lib_ostream.cpp 	new file:   CPP/README.md

---
+++ CPP/Lesson_1/index.cpp
@@ -0,0 +1,26 @@
+/*
+    В C++ iostream, fstream и ostream — это важные компоненты для работы с вводом/выводом
+    input - входные данные
+    output - выходные данные
+    file - работа с файлами
+    rvx <- linux (считывание и изменение данных)
+*/
+
+#include <iostream> // <= подключение библиотеки для работы с вводом/выводом
+#include <fstream>  // <= подключение библиотеки для работы с файлами
+#include <ostream>  // <= подключение библиотеки для работы с выводом
+
+using namespace std; /* 10 MB */
+
+int main() {
+    std::cout << "Hello, World" << std::endl;
+    return 0;
+}
+
+/*
+print("Hello World") -> #include <iostream> 
+                        int main() {
+                            std::cout << "Hello World" << std::endl;
+                        return 0;
+                        }
+*/
---
+++ CPP/Lesson_1/lib_fstream.cpp
@@ -0,0 +1,39 @@
+/*
+    📖 fstream — это библиотека для работы с файлами.
+    
+    ▶️ Она предоставляет три основных класса:
+    1️⃣ std::ifstream: поток ввода из файла
+    2️⃣ std::ofstream: поток вывода в файл
+    3️⃣ std::fstream: поток ввода и вывода в файл
+
+    read - чтение ("r")
+    write - запись ("w")
+*/
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::ofstream outFile("example.txt"); // Открытие файла для записи
+    if (outFile.is_open()) {
+        outFile << "Hello, file :D" << std::endl;
+        outFile.close(); // Закрытие файла
+    } else {
+        std::cerr << "Не удалось открыть файл для записи." << std::endl;
+    }
+
+    std::ifstream inFile("example.txt"); // Открытие файла для чтения
+    std::string line;
+    if (inFile.is_open()) {
+        while (std::getline(inFile, line)) {
+            std::cout << line << std::endl;
+        }
+        inFile.close(); // Закрытие файла
+    } else {
+        std::cerr << "Не удалось открыть файл для чтения." << std::endl;
+    }
+
+    return 0;
+}
